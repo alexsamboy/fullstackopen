93d8780dd6fc03a9e1f7d3da6fb572847b566fa1
alexsamboy <alexsamboy@gmail.com>
2024-09-27 13:57:24 -0400

    PartD

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -102,23 +102,51 @@ const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
   );
 };*/
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const handleLeftClick = () =>
-  setClicks({ ...clicks, left: clicks.left + 1 })
+  const [total, setTotal] = useState(0)
 
-const handleRightClick = () =>
-  setClicks({ ...clicks, right: clicks.right + 1 })
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+
+    setTotal(left + right)
+  }
 
   return (
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {right}
+      <p>{allClicks.join(' ')}</p>
+
+      <History allClicks={allClicks} />
     </div>
   )
 }
