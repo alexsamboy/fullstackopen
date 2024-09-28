b6f4e4ad4fa29c8347b6d993f12c489f57082de1
alexsamboy <alexsamboy@gmail.com>
2024-09-27 22:55:40 -0400

    Unicafe paso 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,26 +14,47 @@ const Button = ({ onClick, text }) => {
   )
 }
 
-const Feedback = ({ text, value }) => {
+const StatisticLine = ({ text, value }) => {
   return (
-    <div>
-      <p>{text} {value}</p>
-    </div>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
 const Statistic = ({ good, neutral, bad }) => {
+
   const total = good + neutral + bad
   const totalPoints = good * 1 + neutral * 0 + bad * -1;
   const average = total ? totalPoints / total : 0
   const positive = good ? good / total : 0
-  return (
-    <p>
-      Total: {total}<br />
-      Average: {average}<br />
-      Positive: {positive}
-    </p>
+
+  if(total === 0){
+    return (
+      <p>No feedback given</p>
     )
+  }else{
+  return (
+    <table>
+      <StatisticLine text='Good' value={good} />
+      <StatisticLine text='Neutral' value={neutral} />
+      <StatisticLine text='Bad' value={bad} />
+      <tr>
+        <td>All:</td>
+        <td>{total}</td>
+      </tr>
+      <tr>
+        <td>Average:</td>
+        <td>{average}</td>
+      </tr>
+      <tr>
+        <td>Positive:</td>
+        <td>{positive * 100} %</td>
+      </tr>
+    </table>
+    )
+  }
 }
 
 
@@ -48,15 +69,13 @@ const App = () => {
   return (
     <div>
       <Header name = {appName} />
+
       <Button onClick={() => setGood(good + 1)} text='Good' />
       <Button onClick={() => setNeutral(neutral + 1)} text='Neutral' />
       <Button onClick={() => setBad(bad + 1)} text='Bad' />
 
-      <h2>Stats</h2>
+      <h2>Statistic</h2>
       
-      <Feedback text='Good' value={good} />
-      <Feedback text='Neutral' value={neutral} />
-      <Feedback text='Bad' value={bad} />
       <Statistic good={good} neutral={neutral} bad={bad} />
       
     </div>
