22af39691bae25d1e2d86a167314569b03611a7a
alexsamboy <alexsamboy@gmail.com>
2024-09-28 11:12:56 -0400

    Courseinfo paso 5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
-
 const Header = (props) => {
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </>
   );
 };
@@ -18,50 +16,16 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part name={props.name} number={props.number} />
+      <Part name={props.parts[0].name} number={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} number={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} number={props.parts[2].exercises} />
     </div>
   );
 };
 const Total = (props) => {
-  return <p>Number of exercises {props.parts1 + props.parts2 + props.parts3}</p>;
+  return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>;
 };
-
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
-    
-  return(
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
-const Display = ({ counter }) => <div>{ counter }</div>
-
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-
-/*const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)
-
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-  const decreaseByOne = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
-
-  const name = 'Peter'
-  const age = 10
+const App = () => {
   const course = {
     name: 'Half Stack application development',
     parts: [
@@ -82,73 +46,11 @@ const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
   return (
     <div>
-
-      <Display counter = { counter } />
-      <Button onClick = {increaseByOne} text='plus' />
-      <Button onClick = {decreaseByOne} text='minus' />
-      <Button onClick = {setToZero} text='Zero' />
-
-      <h1>Greetings</h1>
-      <Hello name = 'Manuel' age = {42} />
-      <Hello name={name} age={age} />
-
-
-      <Header course={course.name} />
-      <Content name={course.parts[0].name} number={course.parts[0].exercises} />
-      <Content name={course.parts[1].name} number={course.parts[1].exercises} />
-      <Content name={course.parts[2].name} number={course.parts[2].exercises} />
-      <Total parts1={course.parts[0].exercises} parts2={course.parts[1].exercises} parts3={course.parts[2].exercises} />
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
-};*/
-
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const [total, setTotal] = useState(0)
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-
-    setTotal(left + right)
-  }
-
-  return (
-    <div>
-      {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-      <p>{allClicks.join(' ')}</p>
-
-      <History allClicks={allClicks} />
-    </div>
-  )
-}
+};
 
 export default App;
