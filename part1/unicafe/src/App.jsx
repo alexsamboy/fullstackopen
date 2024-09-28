62891754c69609f6ae71856652e760234eb84a04
alexsamboy <alexsamboy@gmail.com>
2024-09-27 21:45:07 -0400

    Unicafe paso 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,6 +1,30 @@
 import { useState } from 'react'
 
+const Header = (props) => {
+  return (
+    <header>
+      <h1>{props.name}</h1>
+    </header>
+  )
+};
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const Statistic = ({ text, value }) => {
+  return (
+    <div>
+      <p>{text} {value}</p>
+    </div>
+  )
+}
+
 const App = () => {
+  const appName = 'Give Feedback'
+
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -8,7 +32,14 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Header name = {appName} />
+      <Button onClick={() => setGood(good + 1)} text='Good' />
+      <Button onClick={() => setNeutral(neutral + 1)} text='Neutral' />
+      <Button onClick={() => setBad(bad + 1)} text='Bad' />
+      <h2>Stats</h2>
+      <Statistic text='Good' value={good} />
+      <Statistic text='Neutral' value={neutral} />
+      <Statistic text='Bad' value={bad} />
     </div>
   )
 }
