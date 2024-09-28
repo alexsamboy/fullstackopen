013a4f7b59a6dd6454db2ab563fb709ec7e34bfa
alexsamboy <alexsamboy@gmail.com>
2024-09-27 22:16:01 -0400

    Unicafe paso 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,11 +14,27 @@ const Button = ({ onClick, text }) => {
   )
 }
 
-const Statistic = ({ text, value }) => {
+const Feedback = ({ text, value }) => {
   return (
     <div>
       <p>{text} {value}</p>
     </div>
+  )
+}
+
+const Total = ({ good, neutral, bad }) => {
+  return (
+    <p>
+      Total: {good + neutral + bad}
+    </p>
+    )
+}
+
+const Statistic = ({ text, value }) => {
+  return (
+    <p>
+      {text}: {value}
+    </p>
   )
 }
 
@@ -29,6 +45,8 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
+  const totalPoints = good * 1 + neutral * 0 + bad * -1;
 
   return (
     <div>
@@ -37,9 +55,12 @@ const App = () => {
       <Button onClick={() => setNeutral(neutral + 1)} text='Neutral' />
       <Button onClick={() => setBad(bad + 1)} text='Bad' />
       <h2>Stats</h2>
-      <Statistic text='Good' value={good} />
-      <Statistic text='Neutral' value={neutral} />
-      <Statistic text='Bad' value={bad} />
+      <Feedback text='Good' value={good} />
+      <Feedback text='Neutral' value={neutral} />
+      <Feedback text='Bad' value={bad} />
+      <Total good={good} neutral={neutral} bad={bad} />
+      <Statistic text='Average' value={total ? totalPoints / total : 0} />
+      <Statistic text='Positive' value={good ? good / total : 0} />
     </div>
   )
 }
