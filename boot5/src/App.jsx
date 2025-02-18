0f31cfbf37ef31ddfc614bf083f5c7a1203f7f25
alexsamboy <alexsamboy@gmail.com>
2025-02-18 12:45:48 -0400

    Vite + React + Bootstrap

---
+++ boot5/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='container'>
+      <div className='row'>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React + Bootstrap5</h1>
+      <div className="card bg-info bg-gradient text-white">
+        <button className="btn btn-warning" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ boot5/src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client'
+
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
